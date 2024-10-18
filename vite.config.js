ed3247ded3b9e0ed53ef01fe851ce3587aa1b0d1
Raine92 <raippe_92@hotmail.com>
2024-10-19 02:29:54 +0300

    "Add base property for GitHub Pages deployment"

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: '/bookstore/', // lisää tämä ominaisuus
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  } 
+});
